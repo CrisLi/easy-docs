5bd5528a6a2cffccef556a2be48713aa45466ba8
Chris Li <chrisandkitty@163.com>
2017-04-05 15:13:53 +0800

    Add react-router lib

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import { app } from '../actions';
 import logo from './logo.png';
 import './spinner.css';
@@ -33,6 +36,17 @@ class App extends Component {
           this.props.showSpinner &&
           <div className="loader">Loading...</div>
         }
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </Router>
       </div>
     );
   }
